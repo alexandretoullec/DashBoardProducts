a4009b9d5e5c3d92254dbdafe0cb470fda866d55
[ALexandre Toullec] <[alexandre.toullec@gmail.com]>
2023-11-13 12:34:25 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Home from "./pages/Home/Home";
+import Users from "./pages/users/Users";
+import Products from "./pages/proucts/Porducts";
+
+function App() {
+  return (
+    <div>
+      <Home />
+      <Users />
+      <Products />
+    </div>
+  );
+}
+
+export default App;
